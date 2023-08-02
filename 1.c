a305ecbd4073cb28be96ceb1fb5ffbd2ca6a2536
DhruvTyagi69 <tyagidhruv543@gmail.com>
2023-08-02 20:42:41 +0530

    first program

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int sum = a+b;
+    int difference = a-b;
+    printf("sum is %d\n",sum);
+    printf("difference is %d\n",difference);
+    return 0;
+}
